f936fae34b3104efcf9e7e56a263e38e23f19cb6
shilpadey <shilpadey735@gmail.com>
2022-10-17 17:43:11 +0530

    amount btn

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-    const amount = props.amount;
-    const deleteHandler = () => {
-      console.log("Clicked!!!");
+  const [amount , setTitle] = useState(props.amount);
+    
+    const clickHandler = () => {
+      setTitle('100');
+      console.log(amount);
     }
     
     return (
      <Card className="expense-item">
         <ExpenseDate date = {props.date}/>
         <ExpenseDetails title = {props.title} LocationOfExpenditure = {props.LocationOfExpenditure} amount = {amount}/>
-        <button onClick={deleteHandler}>Delete</button>
+        <button onClick={clickHandler}>Change Amount</button>
      </Card>
     );
 }
