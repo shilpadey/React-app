f49d152bc6afdf476d6c316292bb1f4282dc4403
shilpadey <shilpadey735@gmail.com>
2022-10-18 13:20:05 +0530

    eventhandler updated

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,29 @@ import React from "react";
 import './ExpenseForm.css'
 
 function ExpenseForm() {
+    const titleHandler = (e) => {
+        console.log(e.target.value)
+    };
+    const amountHandler = (e) => {
+        console.log(e.target.value);
+    };
+    const dateHandler = (e) => {
+        console.log(e.target.value);
+    }
     return(
      <form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type = 'text' onChange={e => {console.log(e.target.value)}}/>
+                <input type = 'text' onChange={titleHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type = 'number' min='0.01' step = '0.01' onChange={e => {console.log(e.target.value)}}/>
+                <input type = 'number' min='0.01' step = '0.01' onChange={amountHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type = 'date' onChange={e => {console.log(e.target.value)}}/>
+                <input type = 'date' onChange={dateHandler}/>
             </div>
         </div>
         <div className="new-expense__actions">
